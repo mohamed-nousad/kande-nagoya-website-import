88acb06e2586d905dab596d3ce1fdc2b6af24283
mohamed-nousad <mohamed.nousad@amuryx.com>
2026-09-18 08:50:07 +0530

    unwanted images removed

--- src/components/home/sections/SiteHeader.jsx
+++ src/components/home/sections/SiteHeader.jsx
@@ -15,7 +15,7 @@ export default function SiteHeader({ solid = false }) {
     <header className={`site-header${solid ? " site-header--solid" : ""}`}>
       <div className="header-inner">
         <NavLink className="brand" to="/">
-          <img src="/assets/images/home/logo.png" alt="Wheels Lanka Trading" />
+          <img src="/assets/images/logo/logo.png" alt="Wheels Lanka Trading" />
         </NavLink>
         <nav className="main-nav">
           {NAV.map((item) => (
